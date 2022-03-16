f189d1ecb163a982f02dd4946f251dacc321f656
malicious233 <alfons.hedstrom@gmail.com>
2022-03-16 15:14:14 +0100

    - new colors for bricks, displaying their health

--- src/brick.cpp
+++ src/brick.cpp
@@ -8,7 +8,22 @@ void Brick::draw()
 	if (!alive)
 		return;
 
-	SDL_SetRenderDrawColor(render, 200, 200, 50, 255);
+	RGBAcolor col;
+	switch (health)
+	{
+	case 0:
+		break;
+	case 1:
+		col = { 200, 200, 50, 255 };
+		break;
+	case 2: 
+		col = { 250, 200, 50, 255 };
+		break;
+	case 3:
+		col = { 320, 159, 50, 200 };
+		break;
+	}
+	SDL_SetRenderDrawColor(render, col.r, col.g, col.b, col.a);
 	AABB box = AABB::make_from_position_size(x, y, w, h);
 	draw_box(box);
 }
--- src/engine.h
+++ src/engine.h
@@ -10,6 +10,25 @@ struct KeyState
 	int changeFrame = 0;
 };
 
+extern struct RGBAcolor
+{
+	int r = 0;
+	int g = 0;
+	int b = 0;
+	int a = 0;
+
+	RGBAcolor (int rC, int gC, int bC, int aC) : r(rC), g(gC), b(bC), a(aC)
+	{
+
+	}
+
+	RGBAcolor()
+	{
+
+	}
+
+};
+
 extern KeyState keys[SDL_NUM_SCANCODES];
 bool GetKey(SDL_Scancode key);
 bool GetKeyPressed(SDL_Scancode key);
